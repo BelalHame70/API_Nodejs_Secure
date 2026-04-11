40a38345d910b58e610895093c1a753f34b51360
unknown <belahamed0@gmail.com>
2026-04-11 18:34:54 +0200

    use resend

--- utils/mailService.js
+++ utils/mailService.js
@@ -1,36 +1,18 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 15000
-});
-
-transporter
-  .verify()
-  .then(() => {
-    console.log("Mail transporter is ready");
-  })
-  .catch((err) => {
-    console.error("Mail transporter verify failed:", err);
-  });
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendVerificationEmail = async (email, html, subject) => {
   try {
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev",
       to: email,
       subject,
       html
     });
 
-    console.log("Email sent successfully:", info.messageId);
-    return info;
+    console.log("Email sent successfully:", data);
+    return data;
   } catch (error) {
     console.error("sendVerificationEmail failed:", error);
     throw error;
@@ -39,7 +21,6 @@ const sendVerificationEmail = async (email, html, subject) => {
 
 console.log("NODE_ENV:", process.env.NODE_ENV);
 console.log("WEB_URL:", process.env.WEB_URL);
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+console.log("RESEND_API_KEY exists:", !!process.env.RESEND_API_KEY);
 
 module.exports = { sendVerificationEmail };

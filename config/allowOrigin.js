a3abeb139fe0f5a8c848595b2866faded28ecfb3
unknown <belahamed0@gmail.com>
2026-05-15 21:15:36 +0300

    allow netlify preview origin

--- config/allowOrigin.js
+++ config/allowOrigin.js
@@ -1,7 +1,8 @@
 const allowOrigin = [
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://ageentlab.netlify.app"
+  "https://ageentlab.netlify.app",
+  "https://6a0758f4391b571ae55e8a24--ageentlab.netlify.app"
 ];
 
 module.exports = allowOrigin;

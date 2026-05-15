eb410a4da3d6123443db5e73aca37412e76cedb0
unknown <belahamed0@gmail.com>
2026-05-15 21:03:14 +0300

    all_cors_edit

--- config/allowOrigin.js
+++ config/allowOrigin.js
@@ -1,6 +1,7 @@
 const allowOrigin = [
-//  'http://localhost:3000'
-
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://ageentlab.netlify.app"
 ];
 
 module.exports = allowOrigin;
--- config/cors.js
+++ config/cors.js
@@ -1,6 +1,18 @@
+const allowOrigin = require("./allowOrigin");
+
 const corsOptions = {
-  origin: true,
+  origin(origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowOrigin.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error(`Not allowed by CORS: ${origin}`));
+  },
   credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   optionsSuccessStatus: 200
 };
 
